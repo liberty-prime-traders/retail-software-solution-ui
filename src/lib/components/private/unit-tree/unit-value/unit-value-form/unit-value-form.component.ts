5fc23ef6dab8901ce712f1770baa7e40d4a60753
Ezra Orina <Ezra.Orina@nisc.coop>
2025-04-03 14:40:35 -0500

    RTSS-71 fixed pr comments

--- src/lib/components/private/unit-tree/unit-value/unit-value-form/unit-value-form.component.ts
+++ src/lib/components/private/unit-tree/unit-value/unit-value-form/unit-value-form.component.ts
@@ -1,13 +1,15 @@
-import {Component, computed, inject, input, OnInit} from '@angular/core'
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
-import {InputText} from 'primeng/inputtext'
-import {FormButtonsComponent} from '../../../../reusable/form-buttons/form-buttons.component'
 import {AsyncPipe} from '@angular/common'
-import {FormFieldComponent} from '../../../../reusable/form-field/form-field.component'
+import {Component, computed, inject, input, OnInit} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule, ValidatorFn, Validators} from '@angular/forms'
 import {isNil} from 'lodash-es'
+import {SelectItem} from 'primeng/api'
+import {InputNumber} from 'primeng/inputnumber'
+import {InputText} from 'primeng/inputtext'
+import {Select} from 'primeng/select'
 import {UnitValue} from '../../../../../api/unit-value/unitvalue.model'
 import {UnitValueService} from '../../../../../api/unit-value/unitvalue.service'
-import {Select} from 'primeng/select'
+import {FormButtonsComponent} from '../../../../reusable/form-buttons/form-buttons.component'
+import {FormFieldComponent} from '../../../../reusable/form-field/form-field.component'
 
 @Component({
   standalone: true,
@@ -19,13 +21,14 @@ import {Select} from 'primeng/select'
     FormButtonsComponent,
     AsyncPipe,
     FormFieldComponent,
-    Select
+    Select,
+    InputNumber
   ]
 })
 export class UnitValueFormComponent implements OnInit {
   readonly unitValue = input<UnitValue>()
   readonly unitGroupId = input<string>()
-  readonly baseUnitOptions = input<{ label: string; value: string }[] | null>([])
+  readonly baseUnitOptions = input<Array<SelectItem<string>>>([])
 
   private readonly unitValueService = inject(UnitValueService)
   private readonly formBuilder = inject(FormBuilder)
@@ -33,11 +36,11 @@ export class UnitValueFormComponent implements OnInit {
   readonly unitValueForm = computed(() => this.formBuilder.nonNullable.group({
     id: this.unitValue()?.id,
     name: [this.unitValue()?.name, Validators.required],
-    code: this.unitValue()?.code,
+    code: [this.unitValue()?.code, Validators.required],
     description: this.unitValue()?.description,
     baseUnit: this.unitValue()?.baseUnit,
     conversionFactor: this.unitValue()?.conversionFactor
-  }))
+  }, {validators: this.getDependentFieldsValidator()}))
 
   readonly processingStatus$ = this.unitValueService.processingStatus$()
   readonly failureMessages$ = this.unitValueService.failureMessages$()
@@ -47,7 +50,7 @@ export class UnitValueFormComponent implements OnInit {
   }
 
   resetForm() {
-    this.unitValueForm().reset(this.unitValue())
+    this.unitValueForm().reset()
   }
 
   upsertUnitValue() {
@@ -61,5 +64,18 @@ export class UnitValueFormComponent implements OnInit {
 
   deleteUnitValue() {
     this.unitValueService.delete(this.unitValue()?.id)
+  }
+  
+  private getDependentFieldsValidator(): ValidatorFn {
+    return (formGroup) => {
+      const baseUnit = formGroup.get('baseUnit')?.value
+      const conversionFactor = formGroup.get('conversionFactor')?.value
+      const oneOfTheFieldsIsEmpty = isNil(baseUnit) || isNil(conversionFactor)
+      const oneOfTheFieldsIsNotEmpty = !isNil(baseUnit) || !isNil(conversionFactor)
+      if (oneOfTheFieldsIsEmpty && oneOfTheFieldsIsNotEmpty) {
+        return {required: true}
+      }
+      return null
+    }
   }
 }
