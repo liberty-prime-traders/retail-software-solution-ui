ad03e9bed2d5b7dd2230bf064cf1fa1802de738c
AdongoJr2 <dev.mosesadongo@gmail.com>
2025-04-11 16:24:27 +0300

    Hide `Create a New Organization ` button for users without role `ROLE_CREATE_ORGANIZATION`

--- src/lib/components/private/landing/landing.component.ts
+++ src/lib/components/private/landing/landing.component.ts
@@ -1,11 +1,14 @@
-import {CommonModule} from '@angular/common';
-import {Component, inject} from '@angular/core'
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {RouterLink} from '@angular/router';
-import {FormButtonsComponent} from 'lib/components/reusable/form-buttons/form-buttons.component';
-import {Button} from 'primeng/button';
-import {Card} from 'primeng/card';
-import {InputText} from 'primeng/inputtext';
+import {CommonModule} from '@angular/common'
+import {Component, inject, OnInit} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {RouterLink} from '@angular/router'
+import {SysUserService} from 'lib/api/sys-user/sys-user.service'
+import {FormButtonsComponent} from 'lib/components/reusable/form-buttons/form-buttons.component'
+import {RtsOktaService} from 'lib/utils/services/rts-okta.service'
+import {UserRole} from 'lib/utils/types/user-role.enum'
+import {Button} from 'primeng/button'
+import {Card} from 'primeng/card'
+import {InputText} from 'primeng/inputtext'
 
 @Component({
   standalone: true,
@@ -21,11 +24,19 @@ import {InputText} from 'primeng/inputtext';
     FormButtonsComponent
   ]
 })
-export class LandingComponent {
-  private readonly formBuilder = inject(FormBuilder)
+export class LandingComponent implements OnInit {
+  private readonly userService = inject(SysUserService)
+
+  private readonly rtsOktaService = inject(RtsOktaService)
+  readonly hasCreateRole$ = this.rtsOktaService.hasRole$(UserRole.ROLE_CREATE_ORGANIZATION);
 
   errorMessage = 'Error Message here'
-  hasCreateRole = true
+
+  ngOnInit() {
+    this.userService.post()
+  }
+
+  private readonly formBuilder = inject(FormBuilder)
 
   readonly organizationDomainForm = this.formBuilder.nonNullable.group({
     domain: [null, Validators.required]
