2c771414ca5e1a069acc124178925bd373e8e3ed
AdongoJr2 <dev.mosesadongo@gmail.com>
2025-07-26 11:09:18 +0300

    RTSS 119 Frontend to support migration
    - Update db version form UI (added form validations and UI errors)

--- src/lib/components/db-version/db-version-form/db-version-form.component.ts
+++ src/lib/components/db-version/db-version-form/db-version-form.component.ts
@@ -1,5 +1,12 @@
-import {Component, inject, input, OnInit} from '@angular/core'
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {Component, computed, inject, input, OnInit} from '@angular/core'
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms'
 import {DbVersionService} from '../../../api/db-version/db-version.service'
 import {DbVersion} from '../../../api/db-version/db-version.model'
 import {FormButtonsComponent} from '../../reusable/form-buttons/form-buttons.component'
@@ -24,13 +31,32 @@ export class DbVersionFormComponent implements OnInit {
   private readonly dbVersionService = inject(DbVersionService)
   private readonly formBuilder = inject(FormBuilder)
 
-  readonly dbVersionForm = this.formBuilder.nonNullable.group({
-    versionNumber: ['', Validators.required],
-    prevVersionId: ['']
-  })
-
   readonly processingStatus = this.dbVersionService.selectProcessingStatus
   readonly failureMessages = this.dbVersionService.selectFailureMessages
+
+  readonly hasPreviousVersions = computed(() => this.versions().length > 0)
+
+  readonly dbVersionForm = this.formBuilder.nonNullable.group({
+    versionNumber: ['', [Validators.required, this.versionFormatValidator()]],
+    prevVersionId: ['', [this.previousVersionValidator()]]
+  })
+
+  private versionFormatValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value
+      if (!value) return null
+
+      const pattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}$/
+      return pattern.test(value) ? null : {invalidFormat: true}
+    }
+  }
+
+  private previousVersionValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!this.hasPreviousVersions()) return null
+      return control.value ? null : {required: true}
+    }
+  }
 
   ngOnInit() {
     this.dbVersionService.resetProcessingStatus()
